51f78f140f2ba5b50dd6e3e629e9f7a851073c3d
mstjepan28 <stjepan.markovcic@superius.hr>
2022-06-19 18:44:13 +0200

    selection font set to Arial

--- src/templateEngine/components/PdfBuilder.vue
+++ src/templateEngine/components/PdfBuilder.vue
@@ -318,6 +318,7 @@ export default {
         newSelection.style.overflow = "hidden";
         newSelection.style.pointerEvents = "none"; // needed when shrinking the selection
         newSelection.style.backgroundColor = "rgba(173,216,230, 0.25)";
+        newSelection.style.fontFamily = "Arial";
         newSelection.style.fontSize = "12px";
 
         this.pdfTemplate.appendChild(newSelection);
